fd429b02b5563aa55f2e2bbac8d1cb41d8f1507a
Bipeen swaraj <120006987+bipeenswaraj@users.noreply.github.com>
2024-06-11 22:33:36 +0530

    Update numberGame.cpp

--- numberGame.cpp
+++ numberGame.cpp
@@ -11,19 +11,20 @@ int main() {
     int secretNumber = rand() % 100 + 1;
     int guess;
 
-	for(int i=1;i<=10;i++)
+	for(int i=1;i<=5;i++)
 	{
 		cout<<"Enter a number between 1 to 100: "<<endl;
 		cin>>guess;
 	    if (guess < secretNumber) {
             cout << "Too low! Try again." << endl;
         } else if (guess > secretNumber) {
-            cout << "Too high
-            ! Try again." << endl;
+            cout << "Too high ! Try again." << endl;
         } else {
-            cout << "Congratulations! You guessed the number in "<< endl;
+            cout << "Congratulations! You guessed the number correctly."<< endl;
         break;
 		}
+        cout<< 5 - i<<" attempts left"<<endl;
 }
+    cout<<"your attempt finished,come back later";
     return 0;
 }
